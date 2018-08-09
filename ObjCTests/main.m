61ed8e4b0f4e638ae2390231aecd42af6a4509de
gabriel <gaapalhares@gmail.com>
2018-08-09 16:05:22 -0300

    Initial Commit

---
+++ ObjCTests/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjCTests
+//
+//  Created by Ada 2018 on 09/08/2018.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
